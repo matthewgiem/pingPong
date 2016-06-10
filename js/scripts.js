719fd93c396ec451dd58d5f49c94fb77165507b6
Matthew Giem and Michael Kaiser-Nyman <matthew.giem@gmail.com>
2016-06-10 08:20:50 -0700

    grab the user iput and will be ready to start making your backend logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-/*This file is for your custom js.  All yours*/
+$(document).ready(function() {
+  $('form#input').submit(function(event) {
+    event.preventDefault();
+    var userInput = $('input#number').val();
+    console.log(userInput);
+    $('#result').text();
+  });
+});
