2243292a97742619848a8534267559db901ab1c0
Matthew Giem and Michael Kaiser-Nyman <matthew.giem@gmail.com>
2016-06-10 08:41:00 -0700

    create an array from 1 to user output

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
   $('form#input').submit(function(event) {
     event.preventDefault();
-    var userInput = $('input#number').val();
-    $('#result').text();
+    var userInput = parseInt($('input#number').val());
+    console.log(userInput);
+    pingPong(userInput);
+    // $('#result').text("<li>" + pingPong() "</li>");
   });
 });
+var pingPong = function(number) {
+  var array=[];
+  for (var i = 1; i <= number; i++) {
+    array.push(i)
+  }
+  console.log(array);
+
+}
