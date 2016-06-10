e0334fa7b55d9de37f7982f3befb4d7a1a71d1d5
Matthew Giem and Michael Kaiser-Nyman <matthew.giem@gmail.com>
2016-06-10 08:27:46 -0700

    create the README.md and give the specifications for the project there

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
   $('form#input').submit(function(event) {
     event.preventDefault();
     var userInput = $('input#number').val();
-    console.log(userInput);
     $('#result').text();
   });
 });
